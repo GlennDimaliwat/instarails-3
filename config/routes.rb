68d900d604e67a50798d6e7b96035c8a3018255a
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-10-03 10:29:15 +1100

    Add comments to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,26 @@
 Rails.application.routes.draw do
   
-  
-
+  # Landing Page
   root 'landing#index'
+
+  # Route for Devise
   devise_for :users
+
+  # Route for Users to redirect to Profiles
   resources :users, only: [:show, :update], controller: :profiles
+
+  # Route for Profiles
   resource :profile
+
+  # Route for Photos and Comments
   resources :photos do
     resources :comments
   end
   
+  # Route for Top 10 Trending Users
   get 'trending_users/index', as: "trending_users"
+  
+  # Route for Feed
   # get 'feed/index'
   get '/feed' => 'feed#index', as: "feed"
 
